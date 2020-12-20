02a33a3234a25d1c8593ef3de14c83b3a5d3ebdc
Camilledu25 <camille.giampiccolo@yahoo.fr>
2020-12-20 11:13:41 +0100

    commit avec la table temperature

--- shiny/app.R
+++ shiny/app.R
@@ -15,13 +15,40 @@ library(lubridate)
 
 library(readr)
 bilan_electrique_transpose <- readRDS('data/conso_data.rds')
+temp <- readRDS('data2/temperatures2.rds')
+
 # bilan_electrique_transpose <- read_delim("C:/Users/Camille/Documents/Projet_shiny/bilan-electrique-transpose.csv", 
 #                                          ";", escape_double = FALSE, trim_ws = TRUE)
 
-# renommer les variables
+# renommer les variables conso
+
 colnames(bilan_electrique_transpose) <- c("jour","categorie_client","puissance")
 bilan <- bilan_electrique_transpose[order(bilan_electrique_transpose$jour),]
 bilan$categorie_client=as.factor(bilan$categorie_client)
+
+# renommer les variables temp
+
+temp$Horodate<-as.Date(temp$Horodate, format = "%d/%m/%Y")
+class(temp$Horodate)
+temp$heure<-as.character(temp$heure)
+class(temp$heure)
+temp<-temp%>%filter(heure==as.character("12:00:00"))
+
+temperature_ord <- temp[order(temp$Horodate),]
+
+# ## preparation pour regression
+# 
+# bilan<-bilan%>%filter(jour>=as.Date("2015-12-18"))
+# bilan<-bilan%>%filter(categorie_client=="Entreprises")
+# bilan<-bilan%>%filter(jour<=as.Date("2016-12-18"))
+# 
+# temperature_ord<-temperature_ord%>%filter(Horodate>=as.Date("2015-12-18"))
+# temperature_ord<-temperature_ord%>%filter(Horodate<=as.Date("2016-12-18"))
+# 
+# temperature_only<-temperature_ord%>%select(Horodate,temperature)
+# colnames(temperature_only) <- c("jour","temperature")
+# join<- left_join(bilan, temperature_only)
+
 
 # ui ----------------------------------------------------------------------
 
@@ -90,10 +117,10 @@ server <- function(input, output) {
     
     get_somme <- reactive({
         
-        recupere_donnée <- bilan %>% 
+        recupere_donnee <- bilan %>% 
             filter(categorie_client == input$segment) %>%
             filter(jour < input$obs)
-        somme <- sum(recupere_donnée$puissance)
+        somme <- sum(recupere_donnee$puissance)
         
         somme
         
